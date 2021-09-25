20f4b8929e475e1a618d9404fa73da48c733cb00
D214238 <70898001+D214238@users.noreply.github.com>
2021-09-25 18:02:49 +0200

    [Ticket_2A] closed #4 - typenummers staan nu in een grid.
    typenummers worden nu eerlijk verdeeld en weergegeven in een grid met behulp van bootstrap.
    aangepast bestand: resources/views/pages/type_list.blade.php

--- resources/views/pages/type_list.blade.php
+++ resources/views/pages/type_list.blade.php
@@ -10,14 +10,26 @@
 
 	<p class="brand-manual-discription">{{ __('introduction_texts.type_list', ['brand'=>$brand->name]) }}</p>
 
+	<?php
+        $size = count($types);
+        $columns = 3;
+        $chunk_size = ceil($size / $columns);
+    ?>
+
     <div class="container">
-		<ul>
-		@foreach($types as $type)
-			<li>
-				<a href="/{{ $brand->id }}/{{ $brand->name_url_encoded }}/{{ $type->id }}/{{ $type->name_url_encoded }}/">{{ $type->name }}</a>
-			</li>
-		@endforeach
-		</ul>
+		<div class="row">
+			@foreach($types->chunk($chunk_size) as $chunk)
+			<div class="col-md-4">
+				<ul>
+					@foreach($chunk as $type)
+					<li>
+						<a href="/{{ $brand->id }}/{{ $brand->name_url_encoded }}/{{ $type->id }}/{{ $type->name_url_encoded }}/">{{ $type->name }}</a>
+					</li>
+					@endforeach
+				</ul>
+			</div>
+			@endforeach
+		</div>
 	</div>
 </div>
 @stop
